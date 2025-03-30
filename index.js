59cc94059b647e28bc65e10a7d4e268873c81d27
wabwirevalentine <levaksmaita@gmail.com>
2025-03-30 18:02:06 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,7 @@
+const add = function (item1, item2) {
+  return item1 + item2;
+};
+const square = (x) => x * x;
+const divide = (x, y) => x / y;
+divide(2000, 100);
+console.log(divide(2000, 100));
